0bfdf6e6fa3552f57a1c6bb67543216ec1f637a6
钟意你 <2811499930@qq.com>
2020-07-27 17:03:04 +0800

    Initial commit

---
+++ blog1/src/test/java/cn/lt/blog1/Blog1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lt.blog1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blog1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
