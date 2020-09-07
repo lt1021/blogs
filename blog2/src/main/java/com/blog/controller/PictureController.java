1be9180247c0d86429591736c0926f8c436acd53
Administrator <2811499930@qq.com>
2020-09-07 15:01:55 +0800

    照片

---
+++ blog2/src/main/java/com/blog/controller/PictureController.java
@@ -0,0 +1,16 @@
+package com.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 照片墙
+ */
+@Controller
+public class PictureController {
+    @GetMapping("/picture")
+    public String picture(){
+
+        return "photo";
+    }
+}
