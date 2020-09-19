309ee6ea4bd9ae2e5a7494eb5ed049cc82735a57
Administrator <2811499930@qq.com>
2020-09-19 14:59:10 +0800

    更新

--- blog2/src/main/java/com/blog/controller/MessageShowController.java
+++ blog2/src/main/java/com/blog/controller/MessageShowController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MessageController {
+public class MessageShowController {
     @GetMapping("/message")
     public String message(){
         return "message";
---
+++ blog2/src/main/java/com/blog/controller/admin/PictureController.java
@@ -0,0 +1,54 @@
+package com.blog.controller.admin;
+
+import com.blog.pojo.Picture;
+import com.blog.service.PictureService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * 照片墙
+ */
+@Controller("/admin")
+public class PictureController {
+    @Autowired
+    private PictureService pictureService;
+    @GetMapping("/picture")
+    @ApiOperation(value = "查询所有照片")
+    public String picture(Model model,
+                          @PathVariable Long id, @RequestParam(required = false,defaultValue = "1",value = "pagenum")int pagenum){
+        PageHelper.startPage(pagenum, 5);  //开启分页
+        List<Picture> pictures = pictureService.BlogPicture();
+        PageInfo<Picture> pageInfo = new PageInfo<>(pictures);
+        model.addAttribute("pageInfo",pageInfo);
+        model.addAttribute("pictures",pictures);
+        return "picture";
+    }
+
+
+    @PostMapping("/picture/insertOrUpdate")
+    @ApiOperation(value = "添加一张照片记录")
+    public String insertImage(@Valid Picture picture, RedirectAttributes attributes){
+
+        if ( pictureService.insert(picture) ==1) {
+
+        }
+        attributes.addFlashAttribute("msg", "新增成功");
+        return "redirect :/admin/picture";
+    }
+
+    @DeleteMapping("/picture/delete")
+    @ApiOperation(value = "删除一张照片记录")
+    public String deleteImage(@Valid Picture picture){
+
+        return null;
+    }
+}
