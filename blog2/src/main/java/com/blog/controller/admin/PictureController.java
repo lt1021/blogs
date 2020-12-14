69cdac2cd2f56304a6c4fb67d7e0f969cab14a88
Administrator <2811499930@qq.com>
2020-12-14 18:19:25 +0800

    Squashed commit of the following:
    commit 08f2c40032efe2d5ba57cfb5c41612ae4ba90bca
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Wed Oct 14 00:11:48 2020 +0000
    
        Bump junit from 4.12 to 4.13.1
    
        Bumps [junit](https://github.com/junit-team/junit4) from 4.12 to 4.13.1.
        - [Release notes](https://github.com/junit-team/junit4/releases)
        - [Changelog](https://github.com/junit-team/junit4/blob/main/doc/ReleaseNotes4.12.md)
        - [Commits](https://github.com/junit-team/junit4/compare/r4.12...r4.13.1)
    
        Signed-off-by: dependabot[bot] <support@github.com>
    
    commit 9d0e305151f29eaa72864f5bb49462f96bb49842
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Wed Sep 16 02:50:49 2020 +0000
    
        Bump commons-fileupload from 1.3.1 to 1.3.3 in /blog2
    
        Bumps commons-fileupload from 1.3.1 to 1.3.3.
    
        Signed-off-by: dependabot[bot] <support@github.com>

--- blog2/src/main/java/com/blog/controller/admin/PictureController.java
+++ blog2/src/main/java/com/blog/controller/admin/PictureController.java
@@ -1,28 +1,38 @@
 package com.blog.controller.admin;
 
 import com.blog.pojo.Picture;
+import com.blog.pojo.Tag;
 import com.blog.service.PictureService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 /**
  * 照片墙
  */
-@RestController()
+@Controller()
 @RequestMapping("/admin")
 public class PictureController {
     @Autowired
     private PictureService pictureService;
-    @GetMapping("/picture")
+
+    /**上传地址*/
+    @Value("${file.upload.path}")
+    private String filePath;
+
+    @GetMapping("/pictures")
     @ApiOperation(value = "查询所有照片")
     public String picture(Model model,
                          /* @PathVariable Long id,*/ @RequestParam(required = false,defaultValue = "1",value = "pagenum")int pagenum){
@@ -31,7 +41,40 @@ public class PictureController {
         PageInfo<Picture> pageInfo = new PageInfo<>(pictures);
         model.addAttribute("pageInfo",pageInfo);
         model.addAttribute("pictures",pictures);
-        return "/admin/picture";
+        return "admin/pictures";
+    }
+
+
+    // 执行上传
+    @RequestMapping("upload")
+    public String upload(@RequestParam("file") MultipartFile file, Model model) {
+        // 获取上传文件名
+        String filename = file.getOriginalFilename();
+        // 定义上传文件保存路径
+        String path = filePath+"rotPhoto/";
+        // 新建文件
+        File filepath = new File(path, filename);
+        // 判断路径是否存在，如果不存在就创建一个
+        if (!filepath.getParentFile().exists()) {
+            filepath.getParentFile().mkdirs();
+        }
+        try {
+            // 写入文件
+            file.transferTo(new File(path + File.separator + filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 将src路径发送至html页面
+        model.addAttribute("filename", "/images/rotPhoto/"+filename);
+        return "Page";
+    }
+
+
+    @GetMapping("/pictures/input")
+    @ApiOperation(value = "跳转添加")
+    public String picture(Model model){
+        model.addAttribute("picture", new Picture());//返回一个tag对象给前端th:object
+        return "admin/pictures-input";
     }
 
 
