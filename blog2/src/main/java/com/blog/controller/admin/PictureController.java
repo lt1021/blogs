1c0ac52e2fbde57e8f735c36c289de4e313f6392
Administrator <2811499930@qq.com>
2021-01-29 18:17:09 +0800

    Squashed commit of the following:
    commit 08f2c40032efe2d5ba57cfb5c41612ae4ba90bca
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Wed Oct 14 00:11:48 2020 +0000
    
        Bump junit from 4.12 to 4.13.1
    
        Bumps [junit](https://github.com/junit-team/junit4) from 4.12 to 4.13.1.
        - [Release notes](https://github.com/junit-team/junit4/releases)
        - [Changelog](https://github.com/junit-team/junit4/blob/main/doc/ReleaseNotes4.12.md)
        - [Commits](https://github.com/junit-team/junit4/compare/r4.12...r4.13.1)
    
        Signed-off-by: dependabot[bot] <support@github.com>
    
    commit 9d0e305151f29eaa72864f5bb49462f96bb49842
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Wed Sep 16 02:50:49 2020 +0000
    
        Bump commons-fileupload from 1.3.1 to 1.3.3 in /blog2
    
        Bumps commons-fileupload from 1.3.1 to 1.3.3.
    
        Signed-off-by: dependabot[bot] <support@github.com>

--- blog2/src/main/java/com/blog/controller/admin/PictureController.java
+++ blog2/src/main/java/com/blog/controller/admin/PictureController.java
@@ -1,22 +1,21 @@
 package com.blog.controller.admin;
 
 import com.blog.pojo.Picture;
-import com.blog.pojo.Tag;
 import com.blog.service.PictureService;
+import com.blog.util.FileUpDownload;
+import com.blog.util.StringUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
-import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -28,70 +27,73 @@ public class PictureController {
     @Autowired
     private PictureService pictureService;
 
-//    /**上传地址*/
-//    @Value("${file.upload.path}")
-//    private String filePath;
 
     @GetMapping("/pictures")
     @ApiOperation(value = "查询所有照片")
     public String picture(Model model,
-                         /* @PathVariable Long id,*/ @RequestParam(required = false,defaultValue = "1",value = "pagenum")int pagenum){
+            /* @PathVariable Long id,*/ @RequestParam(required = false, defaultValue = "1", value = "pagenum") int pagenum) {
         PageHelper.startPage(pagenum, 5);  //开启分页
         List<Picture> pictures = pictureService.BlogPicture();
         PageInfo<Picture> pageInfo = new PageInfo<>(pictures);
-        model.addAttribute("pageInfo",pageInfo);
-        model.addAttribute("pictures",pictures);
+        model.addAttribute("pageInfo", pageInfo);
+        model.addAttribute("pictures", pictures);
         return "admin/pictures";
     }
 
 
-
     @GetMapping("/pictures/input")
     @ApiOperation(value = "跳转添加")
-    public String picture(Model model){
+    public String picture(Model model) {
         model.addAttribute("picture", new Picture());//返回一个tag对象给前端th:object
         return "admin/pictures-input";
     }
 
 
-    @PostMapping("/picture/insertOrUpdate")
+    @PostMapping("/picture/insert")
     @ApiOperation(value = "添加一张照片记录")
-    public String insertImage(@Valid Picture picture/*,@RequestParam("file") MultipartFile file*/, RedirectAttributes attributes){
-//        // 获取上传文件名
-//        String filename = file.getOriginalFilename();
-//        // 定义上传文件保存路径
-//        String path = "C:/Users/Administrator/Pictures";
-//        // 新建文件
-//        File filepath = new File(path, filename);
-//        // 判断路径是否存在，如果不存在就创建一个
-//        if (!filepath.getParentFile().exists()) {
-//            filepath.getParentFile().mkdirs();
-//        }
-//        try {
-//            // 写入文件
-//            file.transferTo(new File(path + File.separator + filename));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        picture.setImagePath(filename);
-//
-        if (pictureService.get(picture.getId())!= null&& picture.getId()!=null){
-            pictureService.updatePictureById(picture);
-            attributes.addFlashAttribute("msg", "修改成功");
-        }else {
-            if ( pictureService.insert(picture) == 0  ) {
-                attributes.addFlashAttribute("msg", "新增失败");
-            }
-            attributes.addFlashAttribute("msg", "新增成功");
+    public String insertImage(@Valid Picture picture, RedirectAttributes attributes)throws IOException {
+        if (picture.getPath()==null) {
+            attributes.addFlashAttribute("msg", "图片不能为空");
+            return "redirect :/admin/picture";
         }
-        return "redirect :/admin/picture";
+        Picture pic = FileUpDownload.imagePath(picture);
+        pic.setPicturetime(new Date());
+        if (pictureService.insert(pic) == 0) {
+            attributes.addFlashAttribute("msg", "新增失败");
+        }
+        attributes.addFlashAttribute("msg", "新增成功");
+        return "redirect:/admin/pictures";
+    }
+
+
+    @ApiOperation(value = "跳转照片编辑页面")
+    @GetMapping("/pictures/{id}/input")
+    public String editInput(@PathVariable Integer id, Model model) {
+        model.addAttribute("picture", pictureService.get(id));
+        return "admin/pictures-input";
+    }
+
+    @PostMapping("/picture/insert/{id}")
+    public String editPost(@Valid Picture picture, RedirectAttributes attributes)throws IOException  {
+        Picture pic = picture;
+        if (!StringUtil.isEmpty(picture.getPath().getOriginalFilename())) {
+             pic = FileUpDownload.imagePath(picture);
+        }
+        pic.setPicturetime(new Date());
+        Long P = pictureService.updatePictureById(pic);
+        if (P == 0 ) {
+            attributes.addFlashAttribute("message", "编辑失败");
+        } else {
+            attributes.addFlashAttribute("message", "编辑成功");
+        }
+        return "redirect:/admin/pictures";
     }
 
     @DeleteMapping("/picture/delete/{id}")
     @ApiOperation(value = "删除一张照片记录")
-    public String deleteImage(@PathVariable Integer id,RedirectAttributes attributes){
+    public String deleteImage(@PathVariable Integer id, RedirectAttributes attributes) {
         pictureService.detelePictureById(id);
-        attributes.addFlashAttribute("msg","图片删除成功");
-        return "redirect :/admin/picture";
+        attributes.addFlashAttribute("msg", "图片删除成功");
+        return "redirect:/admin/pictures";
     }
 }
