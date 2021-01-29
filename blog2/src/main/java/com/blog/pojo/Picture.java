715117c4a0e8475f27886ebd6b979ddd4688e80e
Administrator <2811499930@qq.com>
2021-01-29 18:21:25 +0800

    Squashed commit of the following:
    commit 08f2c40032efe2d5ba57cfb5c41612ae4ba90bca
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Wed Oct 14 00:11:48 2020 +0000
    
        Bump junit from 4.12 to 4.13.1
    
        Bumps [junit](https://github.com/junit-team/junit4) from 4.12 to 4.13.1.
        - [Release notes](https://github.com/junit-team/junit4/releases)
        - [Changelog](https://github.com/junit-team/junit4/blob/main/doc/ReleaseNotes4.12.md)
        - [Commits](https://github.com/junit-team/junit4/compare/r4.12...r4.13.1)
    
        Signed-off-by: dependabot[bot] <support@github.com>
    
    commit 9d0e305151f29eaa72864f5bb49462f96bb49842
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Wed Sep 16 02:50:49 2020 +0000
    
        Bump commons-fileupload from 1.3.1 to 1.3.3 in /blog2
    
        Bumps commons-fileupload from 1.3.1 to 1.3.3.
    
        Signed-off-by: dependabot[bot] <support@github.com>

--- blog2/src/main/java/com/blog/pojo/Picture.java
+++ blog2/src/main/java/com/blog/pojo/Picture.java
@@ -3,8 +3,9 @@ package com.blog.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotBlank;
+import java.util.Date;
 
 /**
  * 照片墙实体类
@@ -15,10 +16,11 @@ import javax.validation.constraints.NotBlank;
 public class Picture {
     private Integer id;
     private String picturename;//照片名字
-    private String picturetime;//照片时间
-    private String pictureaddress;//照片地址
+    private Date picturetime;//照片时间
     private String picturedescription;//照片描述
-    @NotBlank(message = "请上传图片")
     private String imagePath; //图片路径
+    private MultipartFile path; //图片路径
+
+
 
 }
