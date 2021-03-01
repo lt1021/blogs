08949d49053630f7a0155efb7f33bfbc386b38f8
Administrator <2811499930@qq.com>
2021-03-01 22:50:13 +0800

    Squashed commit of the following:
    commit 08f2c40032efe2d5ba57cfb5c41612ae4ba90bca
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Wed Oct 14 00:11:48 2020 +0000
    
        Bump junit from 4.12 to 4.13.1
    
        Bumps [junit](https://github.com/junit-team/junit4) from 4.12 to 4.13.1.
        - [Release notes](https://github.com/junit-team/junit4/releases)
        - [Changelog](https://github.com/junit-team/junit4/blob/main/doc/ReleaseNotes4.12.md)
        - [Commits](https://github.com/junit-team/junit4/compare/r4.12...r4.13.1)
    
        Signed-off-by: dependabot[bot] <support@github.com>
    
    commit 9d0e305151f29eaa72864f5bb49462f96bb49842
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Wed Sep 16 02:50:49 2020 +0000
    
        Bump commons-fileupload from 1.3.1 to 1.3.3 in /blog2
    
        Bumps commons-fileupload from 1.3.1 to 1.3.3.
    
        Signed-off-by: dependabot[bot] <support@github.com>

--- blog2/src/main/java/com/blog/excel/test/util/TestFileUtil.java
+++ blog2/src/main/java/com/blog/excel/test/util/TestFileUtil.java
@@ -1,5 +1,6 @@
 package com.blog.excel.test.util;
 
+import java.io.File;
 import java.io.InputStream;
 
 /**
@@ -20,5 +21,25 @@ public class TestFileUtil {
     }
     public static String getImage(){
         return  "E:\\data\\image\\";
+    }
+
+    public static File createNewFile(String pathName) {
+        File file = new File(getPath() + pathName);
+        if (file.exists()) {
+            file.delete();
+        } else {
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+        }
+        return file;
+    }
+
+    public static File readFile(String pathName) {
+        return new File(getPath() + pathName);
+    }
+
+    public static File readUserHomeFile(String pathName) {
+        return new File(System.getProperty("user.home") + File.separator + pathName);
     }
 }
---
+++ blog2/src/main/java/com/blog/excel/test/util/demo/fill/FillData.java
@@ -0,0 +1,12 @@
+package com.blog.excel.test.util.demo.fill;
+
+import lombok.Data;
+
+/**
+ * @author Jiaju Zhuang
+ */
+@Data
+public class FillData {
+    private String name;
+    private double number;
+}
