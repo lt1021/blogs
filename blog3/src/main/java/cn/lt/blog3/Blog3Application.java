f0c071ebe474e240ea68c3ea532ffeee2e72b5a8
“lt1021 <2811499930@qq.com>
2021-04-30 18:12:30 +0800

    提交

--- blog3/src/main/java/cn/lt/blog3/Blog3Application.java
+++ blog3/src/main/java/cn/lt/blog3/Blog3Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
-@MapperScan("cn.lt.blog3.mapper.*")
+@MapperScan("cn.lt.blog3.mapper")
 public class Blog3Application {
 
     public static void main(String[] args) {
